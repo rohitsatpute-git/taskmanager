de06ed7dd639f9ea5773b6edc147bb18ba508062
rohitsatpute-git <satputerohit00@gmail.com>
2025-02-15 22:01:22 +0530

    fix: allow vercel to make api request

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,26 +2,35 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
     const token = req.cookies.get("token")?.value;
-    console.log("token", token)
+    console.log("token", token);
 
     if (!token) {
-        return NextResponse.redirect(new URL("/login", req.url)); 
+        return NextResponse.redirect(new URL("/login", req.url));
     }
 
-    try {        
+    try {
         const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+        
+        const { payload: user } = await jwtVerify(token, secret);
 
-        const user = jwtVerify(token, secret); 
-        req.headers.set("x-user", JSON.stringify(user)); 
+        const requestHeaders = new Headers(req.headers);
+        requestHeaders.set("x-user", JSON.stringify(user));
+
+        const res = NextResponse.next({
+            request: { headers: requestHeaders },
+        });
+
+        res.headers.append("Access-Control-Allow-Origin", "https://taskmanager-sigma-six.vercel.app/");
+
+        return res;
     } catch (error) {
-        console.log("error", error)
-        return NextResponse.redirect(new URL("/login", req.url)); 
+        console.log("error", error);
+        return NextResponse.redirect(new URL("/login", req.url));
     }
-
-    return NextResponse.next(); 
 }
+
 
 export const config = {
     matcher: ["/"], 
